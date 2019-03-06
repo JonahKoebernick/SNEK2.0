e64ce28808a54ac49f7d1960a51f97c7270cb9f6
JonahKoebernick <37969436+JonahKoebernick@users.noreply.github.com>
2019-03-05 21:14:46 -0800

    Update board.py

--- app/board.py
+++ app/board.py
@@ -22,7 +22,7 @@ def update_board(state):
         snake_body = snake['body']
         for coord in snake_body[1:]:
             Matrix[coord['y']][coord['x']] = OCCUPIED
-        Tail_coord = snake_body[len(snake_body)]
+        Tail_coord = snake_body[len(snake_body)-1]
         Matrix[Tail_coord['y']][Tail_coord['x']] = UNOCCUPIED
         head_coord = snake_body[0]
         Matrix[head_coord['y']][head_coord['x']] = HEAD
